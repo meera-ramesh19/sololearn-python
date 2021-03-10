c7d1412a0f444c7019345a111832b130aab3d0ae
meera-ramesh19 <46873861+meera-ramesh19@users.noreply.github.com>
2021-03-10 11:25:46 -0500

    Create converting timetolocaltime.py

---
+++ converting timetolocaltime.py
@@ -0,0 +1,19 @@
+# Created by Meera 
+program about time 
+
+
+
+from time import ctime
+from time import strftime
+
+print(ctime())
+
+print(strftime("%H:%M:%S"))
+
+print(strftime("%a:%A:%w:%W"))
+
+print(strftime("%d:%m:%b:%B"))
+
+print(strftime("%y:%Y:%j"))
+
+print(strftime("%H:%I:%p:%M:%S"))
