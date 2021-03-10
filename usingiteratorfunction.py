40ac38d255105e0c17f6681814ff53a2fe2990ef
meera-ramesh19 <46873861+meera-ramesh19@users.noreply.github.com>
2021-03-10 11:37:56 -0500

    Create usingiteratorfunction.py

---
+++ usingiteratorfunction.py
@@ -0,0 +1,17 @@
+#create list of items using string or number and create an iterator using
+#iterfunction  
+
+
+my_list = ["monday","tuesday", "wednesday","thursday", "friday","saturday","sunday"]
+
+my_iterator = iter(my_list)
+
+for i in range(0, len(my_list)):
+    
+     next_item=next(my_iterator)
+     
+     print(next_item)
+     
+for i in my_list:
+    print(i)
+
