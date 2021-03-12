31ff0d3e594e360aa907fbb9bde9fd261e7ff7d2
meera-ramesh19 <46873861+meera-ramesh19@users.noreply.github.com>
2021-03-11 22:40:18 -0500

    Create listcreationprogram.py

---
+++ listcreationprogram.py
@@ -0,0 +1,30 @@
+#Python program to demonstrate  
+# Creation of List  
+  
+ 
+List = [] 
+print("Intial blank List: ") 
+print(List) 
+  
+List = ['GeeksForGeeks'] 
+print("\nList with the use of String: ") 
+print(List) 
+ 
+List = ["Geeks", "For", "Geeks"] 
+print("\nList containing multiple values: ") 
+print(List[0])  
+print(List[2]) 
+  
+ 
+List = [['Geeks', 'For'] , ['Geeks']] 
+print("\nMulti-Dimensional List: ") 
+print(List) 
+
+List = [1, 2, 4, 4, 3, 3, 3, 6, 5] 
+print("\nList with the use of Numbers: ") 
+print(List) 
+  
+ 
+List = [1, 2, 'Geeks', 4, 'For', 6, 'Geeks'] 
+print("\nList with the use of Mixed Values: ") 
+print(List) 
